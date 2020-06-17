036ac54f5cf0bd46f04fa081daa849c83295c0bf
HakuSystems <66133638+HakuSystems@users.noreply.github.com>
2020-06-17 12:36:51 +0200

    Visual Basic Updater

---
+++ nanoDiscLauncher/Application.xaml.vb
@@ -0,0 +1,65 @@
+﻿Imports System.IO
+Imports System.Net
+Class Application
+    ReadOnly clientName = "nanoclient"
+    ReadOnly clientExtension = ".exe"
+    ReadOnly apiURL = "https://www.notlyze.de/api.php?call="
+
+    Private clientPath As String
+    Private Sub OnStart()
+        'Logo mit progress_bar bei Download.
+        Init()
+
+        Process.Start(clientPath)
+        Shutdown()
+    End Sub
+
+    Private Sub Init()
+        Dim dataFolder = Path.GetTempPath
+
+        clientPath = dataFolder & clientName & clientExtension
+
+        If Directory.Exists(dataFolder) Then
+            If File.Exists(clientPath) Then
+                CheckClient(clientPath)
+            Else
+                'Not yet downloaded.
+                DownloadClient()
+            End If
+        Else
+            Directory.CreateDirectory(dataFolder)
+        End If
+    End Sub
+
+    Private Sub DownloadClient()
+        Try
+            Using client As New WebClient
+                client.DownloadFile(apiURL & "download", clientPath)
+            End Using
+        Catch
+            MessageBox.Show("Something went wrong at downloading files.", "Launcher", MessageBoxButton.OK, MessageBoxImage.Error)
+            Environment.Exit(1)
+            Return
+        End Try
+    End Sub
+
+    Private Sub CheckClient(clientPath As String)
+        Dim clientHash = ConvertToHex(FileHash(clientPath))
+
+        Dim latestHash
+        Using client As New WebClient
+            Try
+                latestHash = client.DownloadString(apiURL & "hash")
+            Catch
+                MessageBox.Show("Something went wrong at fetching data.", "Launcher", MessageBoxButton.OK, MessageBoxImage.Error)
+                Environment.Exit(1)
+                Return
+            End Try
+        End Using
+
+        If Not clientHash = latestHash Then
+            'Update!
+            DownloadClient()
+        End If
+    End Sub
+End Class
---
+++ nanoDiscLauncher/Helper.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Security.Cryptography
+Imports System.IO
+
+Module Helper
+    Public Function FileHash(filePath As String)
+        Dim fileBytes() As Byte = File.ReadAllBytes(filePath)
+        Dim targetCrypto As New MD5CryptoServiceProvider()
+        Dim byteHash() As Byte = targetCrypto.ComputeHash(fileBytes)
+
+        Return Convert.ToBase64String(byteHash)
+    End Function
+
+    Public Function ConvertToHex(baseString As String) As String
+        Dim b64bytes() As Byte = System.Convert.FromBase64String(baseString)
+
+        Dim ret As String
+        For Each b As Byte In b64bytes
+            ret &= Hex(b).PadLeft(2, "0"c)
+        Next
+
+        Return ret.ToLower
+    End Function
+End Module
